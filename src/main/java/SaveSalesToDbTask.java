c34cbe5efc8bcb80b07a47efe91e9601a4ad8304
Maciej <gorski.maciej@o2.pl>
2021-01-15 00:45:21 +0100

    working (i think) version of progress dialog showing save progress

--- src/main/java/SaveSalesToDbTask.java
+++ src/main/java/SaveSalesToDbTask.java
@@ -1,9 +1,14 @@
 import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.scene.control.Alert;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class SaveSalesToDbTask extends Task<Boolean> {
 
@@ -16,7 +21,34 @@ public class SaveSalesToDbTask extends Task<Boolean> {
 
     @Override
     protected Boolean call() throws Exception {
-
-        return null;
+        Session session = ApplicationDatabase.getNewSession();
+        Transaction transaction = null;
+        try {
+            transaction = session.beginTransaction();
+            AtomicInteger currentWork = new AtomicInteger(0);
+            final int totalWork = sales.size();
+            sales.forEach(sale -> {
+                var item = sale.getItem();
+                item.setSale(sale);
+                var currencies = sale.getCurrencies();
+                session.save(sale);
+                session.save(item);
+                currencies.forEach(c -> {
+                    c.setSale(sale);
+                    session.save(c);
+                });
+                updateProgress(currentWork.incrementAndGet(), totalWork);
+            });
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw e;
+        } finally {
+            session.close();
+        }
+        return true;
     }
+
 }
